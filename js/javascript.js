d7dd897ae9a38d72f8678d8540ee737e2df259c5
AdyPaulino <ariadiny.miranda@gmail.com>
2015-11-03 09:33:15 -0500

    Pictures
    Pictures added randomly

--- js/javascript.js
+++ js/javascript.js
@@ -1,41 +1,82 @@
 $(document).ready(function() {
-  var $listNode = $("#image_list_game");
-  var $listCache = $("#image_list");
-  var $imageNode = $("#image");
-  var links = $listNode.find('a');
-  var linksCache = $listCache.find('a');
+    var $listSpaces = $("#image_list_game");
+    var $listPictures = $("#image_list");
+    var linksSpaces = $listSpaces.find('a');
+    var linksPicture = $listPictures.find('a');
 
-  var i, linkNode,linkCache, image;
-  var imageCache = [];
-  var imageTurned = [];
-  var imageCounter = 0;
+    var i, linkSpace,linkPicture, image;
+    var imageCache = [];
+    var imageTurned = [];
+    var imageRandom = [];
+    var imageCounter = 0;
 
-  for(i = 0; i < links.length; i++){
-    linkNode = links[i];
-    linkCache = linksCache[i];
-
-    $(linkNode).on('click', function(e) {
-      e.preventDefault();
-      imageCounter = $(this).parent().index();
-      imageTurned[imageCounter] = true;
-      swapImage(imageCounter);
-    });
-
-    image = new Image();
-    image.src = $(linkCache).attr('href');
-    image.title = $(linkCache).attr('title');
-    imageCache.push(image);
-  }
-
-  function swapImage(count) {
-    image = imageCache[count];
-    if (imageTurned[count] == true){
-        document.getElementById(count+1).src = image.src;
-        $imageNode.attr('alt', image.title);
-    }else {
-        alert('missed');
+    //CONITNUE for all spaces!!!!
+    for(i = 0; i < linksPicture.length; i++){
+        //randomly place the pictures
+        var times = linksSpaces.length / 2;
+        do {
+            var place = Math.floor((Math.random() * linksSpaces.length) + 0);
+            if (imageRandom[place] == true){
+                continue;
+            }
+            linkPicture = linksPicture[i];
+            image = new Image();
+            image.src = $(linkPicture).attr('href');
+            image.title = $(linkPicture).attr('title');
+            imageCache[place] = image;
+            imageRandom[place] = true;
+            times--;
+        } while (times > 0);   
+        
+        linkSpace = linksSpaces[i];
+        $(linkSpace).on('click', function(e) {
+            e.preventDefault();
+            imageCounter = $(this).parent().index();
+            //mark the image as turned
+            image = imageCache[imageCounter];
+            imageTurned[image.title] = true;
+            swapImage(imageCounter);
+        });
+        
+        function swapImage(count) {
+            image = imageCache[count];
+            if (imageTurned[image.title] == true){
+                document.getElementById(count+1).src = image.src;
+                //$imageNode.attr('alt', image.title);
+            }else {
+                alert('missed');
+            }
+        }
     }
     
-  }
+    
+    
+   /* for(i = 0; i < linksSpaces.length; i++){
+        linkSpace = linksSpaces[i];
+        //linkPicture = linksPicture[i];
+
+        $(linkSpace).on('click', function(e) {
+            e.preventDefault();
+            imageCounter = $(this).parent().index();
+            imageTurned[imageCounter] = true;
+            swapImage(imageCounter);
+        });
+
+        image = new Image();
+        image.src = $(linkPicture).attr('href');
+        image.title = $(linkPicture).attr('title');
+        imageCache.push(image);
+    }
+
+    function swapImage(count) {
+        image = imageCache[count];
+        if (imageTurned[count] == true){
+            document.getElementById(count+1).src = image.src;
+            $imageNode.attr('alt', image.title);
+        }else {
+            alert('missed');
+        }
+
+    } */
 
 });
