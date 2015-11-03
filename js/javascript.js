d102ddd9a7f7586ba07a70b5acba5a103b36e66a
AdyPaulino <ariadiny.miranda@gmail.com>
2015-11-03 16:23:23 -0500

    working game
    the matching and the wrong choices are shown/hidden properly.

--- js/javascript.js
+++ js/javascript.js
@@ -6,16 +6,20 @@ $(document).ready(function() {
 
     var i, linkSpace,linkPicture, image;
     var imageCache = [];
-    var imageTurned = [];
+    var imagesTurned = 0;
     var imageRandom = [];
     var imageCounter = 0;
+    var lastImageTurned = 0;
+    var lastSpaceTurned = 0;
 
-    //CONITNUE for all spaces!!!!
     for(i = 0; i < linksPicture.length; i++){
-        //randomly place the pictures
+        // how many places each picture will appear - it is always two of each picture
         var times = linksSpaces.length / 2;
+        
+        //randomly placing the pictures
         do {
             var place = Math.floor((Math.random() * linksSpaces.length) + 0);
+            //if there is an image in that position just get another position
             if (imageRandom[place] == true){
                 continue;
             }
@@ -28,55 +32,46 @@ $(document).ready(function() {
             times--;
         } while (times > 0);   
         
+    }
+        
+    for(i = 0; i < linksSpaces.length; i++){
         linkSpace = linksSpaces[i];
+        
         $(linkSpace).on('click', function(e) {
             e.preventDefault();
             imageCounter = $(this).parent().index();
-            //mark the image as turned
-            image = imageCache[imageCounter];
-            imageTurned[image.title] = true;
+
             swapImage(imageCounter);
         });
         
         function swapImage(count) {
             image = imageCache[count];
-            if (imageTurned[image.title] == true){
+
+            if (lastImageTurned == 0){
+                lastImageTurned = image.title;
+            }
+            
+            //if the images are the same, can turn
+            if (lastImageTurned == image.title){
                 document.getElementById(count+1).src = image.src;
-                //$imageNode.attr('alt', image.title);
+                
+                imagesTurned++;
+                if (imagesTurned > 1){
+                    //another pair will be evaluated
+                    lastImageTurned = 0;
+                    imagesTurned = 0;
+                }
+                
+                //save the first image turned just in case the next is a wrong one
+                lastSpaceTurned = count+1;
             }else {
-                alert('missed');
+                //if it's the wrong choice, just turn the actual and the previous one back
+                document.getElementById(count+1).src = linkSpace.href;
+                document.getElementById(lastSpaceTurned).src = linkSpace.href;
+                lastImageTurned = 0;
+                imagesTurned = 0;
             }
         }
     }
-    
-    
-    
-   /* for(i = 0; i < linksSpaces.length; i++){
-        linkSpace = linksSpaces[i];
-        //linkPicture = linksPicture[i];
-
-        $(linkSpace).on('click', function(e) {
-            e.preventDefault();
-            imageCounter = $(this).parent().index();
-            imageTurned[imageCounter] = true;
-            swapImage(imageCounter);
-        });
-
-        image = new Image();
-        image.src = $(linkPicture).attr('href');
-        image.title = $(linkPicture).attr('title');
-        imageCache.push(image);
-    }
-
-    function swapImage(count) {
-        image = imageCache[count];
-        if (imageTurned[count] == true){
-            document.getElementById(count+1).src = image.src;
-            $imageNode.attr('alt', image.title);
-        }else {
-            alert('missed');
-        }
-
-    } */
 
 });
